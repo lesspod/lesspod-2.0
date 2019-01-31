231f5de44e8006487ba1d1f55fb51b81d8b9c9b7
Rajan Chandi <chandi.rajan@gmail.com>
2019-01-31 11:26:34 +0530

    fix for mongo deprecation warning

--- server/db.js
+++ server/db.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose')
 
 let uri = process.env.DB_URI || 'mongodb://localhost:27017/lptest'
 module.exports = () => {
-  mongoose.connect(uri)
+  // mongoose.connect(uri)
+
+  mongoose.connect(uri, { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err
+    console.log('Database online')
+  })
 
   mongoose.connection.on('error', err => {
     console.log('mongo connection error', err)
