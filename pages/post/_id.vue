94902e6dddb9d278e4f7228703d9b246654593f9
Rajan Chandi <chandi.rajan@gmail.com>
2019-02-25 20:02:54 +0530

    added disqus comments

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -21,6 +21,7 @@
               v-quill:myQuillEditor="editorOption"
             ></div>
             <br>
+            <div class="content" id="disqus_thread"></div>
             <!-- <textarea
               id="inline-post-content"
               v-model="content"
@@ -60,9 +61,22 @@ export default {
     this.myQuillEditor.disable()
     this.post_id = this.$nuxt._route.params.id
     this.$store.dispatch('posts/GET_POST', this.post_id)
-    // setTimeout(() => {
-    //   this.content = 'i am changed'
-    // }, 3000)
+
+    // load disqus comments below
+    /**
+    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+    var disqus_config = function () {
+    this.page.url = window.location;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = this.href.substr(this.href.lastIndexOf('/') + 1); // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    };
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://lesspod.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
   },
   computed: {
     menus() {
