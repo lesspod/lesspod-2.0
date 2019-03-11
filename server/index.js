9bb02f2d43c6547657769e7e882f8ae3328a9806
Rajan Chandi <chandi.rajan@gmail.com>
2019-03-11 14:55:33 +0530

    code cleanup

--- server/index.js
+++ server/index.js
@@ -49,15 +49,6 @@ async function start() {
   console.log('API routes: ' + JSON.stringify(routes))
   app.use("/api", [cors(), bodyParser.json()], routes);
 
-  // POST `/api/login` to log in the user and add him to the `req.session.authUser`
-  app.post('/api/login', function (req, res) {
-    if (req.body.username === 'demo' && req.body.password === 'demo') {
-      req.session.authUser = { username: 'demo' }
-      return res.json({ username: 'demo' })
-    }
-    res.status(401).json({ error: 'Bad credentials' })
-  });
-
   // POST `/api/logout` to log out the user and remove it from the `req.session`
   app.post('/api/logout', function (req, res) {
     delete req.session.authUser
--- store/index.js
+++ store/index.js
@@ -1,69 +1,3 @@
-// import Vue from 'vue'
-// import Vuex from 'vuex'
-
-// Vue.use(Vuex)
-
-// Polyfill for `window.fetch()`
-// require('whatwg-fetch')
-
-// const store = () => new Vuex.Store({
-
-//   state: () => ({
-//     authUser: null
-//   }),
-
-//   mutations: {
-//     SET_USER: function (state, user) {
-//       state.authUser = user
-//     }
-//   },
-
-//   actions: {
-//     login ({ commit }, { username, password }) {
-//       return fetch('/api/login', {
-//         // Send the client cookies to the server
-//         credentials: 'same-origin',
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({
-//           username,
-//           password
-//         })
-//       })
-//       .then((res) => {
-//         if (res.status === 401) {
-//           throw new Error('Bad credentials')
-//         } else {
-//           return res.json()
-//         }
-//       })
-//       .then((authUser) => {
-//         commit('SET_USER', authUser)
-//       })
-//     },
-//     logout ({ commit }) {
-//       return fetch('/api/logout', {
-//         // Send the client cookies to the server
-//         credentials: 'same-origin',
-//         method: 'POST'
-//       })
-//       .then(() => {
-//         commit('SET_USER', null)
-//       })
-//     },
-//     async nuxtServerInit ({ commit, dispatch }, { req }) {
-//       await dispatch('menus/GET_MENUS')
-//       if (req.session && req.session.authUser) {
-//         commit('SET_USER', req.session.authUser)
-//       }
-//     }
-//   }
-// })
-
-// export default store
-
 export const state = () => ({
   authUser: null
 })
@@ -146,7 +80,4 @@ export const actions = {
       commit('SET_USER', req.session.authUser)
     }
   }
-  // async nuxtServerInit ({ dispatch }) {
-  //   await dispatch('menus/GET_MENUS')
-  // }
 }
