3c6c26bc0c7c6965978e01c3fa5194c9cc1a95c1
Rajan Chandi <chandi.rajan@gmail.com>
2019-04-09 20:02:31 +0530

    revert + fix

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-let uri = process.env.DB_URI || "mongodb://localhost:27017/lptest";
+let uri = process.env.DB_URI || 'mongodb://localhost:27017/';
 module.exports = () => {
   // mongoose.connect(uri)
 
-  mongoose.connect(uri, { useNewUrlParser: true }, (err, res) => {
+  mongoose.connect(uri, { useNewUrlParser: true, dbName: 'lptest', keepAlive: true, poolSize: 5 }, (err, res) => {
     if (err) throw err;
     console.log("Database online");
   });
